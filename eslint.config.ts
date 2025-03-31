7e2c8420255e08d013330dcff7a590fe9ed8a3c7
aashovkin <artemshshovkin@yandex.ru>
2025-03-31 23:37:03 +0300

    chore: пофикшен тупой наезд от линтера

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,5 +3,5 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   vue: true,
   typescript: true,
-  ignores: ['.github/**'],
+  ignores: ['.github/**', 'dist/**', 'node_modules/**', '**/__tests__/**'],
 })
