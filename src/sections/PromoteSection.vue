b309ada1dc22f3dc07bc4085c7d0ed146573fbb6
Aleksandr Korotaev <lekzd@mail.ru>
2025-04-28 13:11:30 +0300

    feat(promote): add background animation

--- src/sections/PromoteSection.vue
+++ src/sections/PromoteSection.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import logo from '@/assets/logo.png'
 import Button from '@/components/ui/Button.vue'
+import PromoteBackground from '@/components/PromoteBackground.vue'
 import Laptop from '~icons/lucide/laptop'
 import MessageSquare from '~icons/lucide/message-square'
 import Users from '~icons/lucide/users'
 </script>
 
 <template>
-  <section class="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-b from-background to-muted">
-    <div class="container px-4 md:px-6 space-y-10 xl:space-y-16">
+  <section class="relative w-full py-12 md:py-24 lg:py-32 bg-gradient-to-b from-background to-muted">
+    <PromoteBackground />
+    <div class="relative container px-4 md:px-6 space-y-10 xl:space-y-16">
       <div class="grid gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
-        <div class="flex flex-col justify-center space-y-4">
+        <div class="flex flex-col justify-center space-y-4 border bg-white bg-opacity-50 backdrop-blur-sm rounded-lg p-4">
           <div class="space-y-2">
             <h1 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
               Станьте хозяином в мире IT
@@ -33,13 +35,13 @@ import Users from '~icons/lucide/users'
           </div>
         </div>
         <div class="flex items-center justify-center">
-          <img
+          <!-- <img
             :src="logo" alt="IT-ХОЗЯЕВА сообщество" class="rounded-lg object-contain"
-          >
+          > -->
         </div>
       </div>
       <div class="mx-auto grid items-center gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-3 md:gap-8">
-        <div class="flex flex-col items-center space-y-2 border rounded-lg p-4">
+        <div class="flex flex-col items-center space-y-2 border rounded-lg p-4 bg-white bg-opacity-50 backdrop-blur-sm">
           <Users class="h-8 w-8 text-primary" />
           <h3 class="text-xl font-bold">
             200+
@@ -48,7 +50,7 @@ import Users from '~icons/lucide/users'
             Активных участников
           </p>
         </div>
-        <div class="flex flex-col items-center space-y-2 border rounded-lg p-4">
+        <div class="flex flex-col items-center space-y-2 border rounded-lg p-4 bg-white bg-opacity-50 backdrop-blur-sm">
           <MessageSquare class="h-8 w-8 text-primary" />
           <h3 class="text-xl font-bold">
             1000+
@@ -57,7 +59,7 @@ import Users from '~icons/lucide/users'
             Сообщений ежедневно
           </p>
         </div>
-        <div class="flex flex-col items-center space-y-2 border rounded-lg p-4">
+        <div class="flex flex-col items-center space-y-2 border rounded-lg p-4 bg-white bg-opacity-50 backdrop-blur-sm">
           <Laptop class="h-8 w-8 text-primary" />
           <h3 class="text-xl font-bold">
             20+
